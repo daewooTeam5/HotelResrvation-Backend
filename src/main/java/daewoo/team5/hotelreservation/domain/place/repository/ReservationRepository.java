3074e38fc5b3e56de72745d1646e9500a75746b3
devaspirant0510 <seungho020510@gmail.com>
2025-09-22 16:32:14 +0900

    fix: merge conflict

--- src/main/java/daewoo/team5/hotelreservation/domain/place/repository/ReservationRepository.java
+++ src/main/java/daewoo/team5/hotelreservation/domain/place/repository/ReservationRepository.java
@@ -1,6 +1,7 @@
 package daewoo.team5.hotelreservation.domain.place.repository;
 
 import daewoo.team5.hotelreservation.domain.payment.entity.Reservation;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -16,6 +17,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long>,
     // 예약 ID로 예약을 조회
     Optional<Reservation> findById(Long reservationId);
 
+    Optional<Reservation> findByOrderId(String orderId);
 
     @Query("select r from Reservation r join fetch r.room join fetch r.guest where r.reservationId = :reservationId")
     Optional<Reservation> findByIdFetchJoin(Long reservationId);
