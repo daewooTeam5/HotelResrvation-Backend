9c80022a5c3159c8f247c1ac7a57ac855503590e
devaspirant0510 <seungho020510@gmail.com>
2025-09-24 20:57:24 +0900

    feat: 쿠폰 관리 기능 추가

---
+++ src/main/java/daewoo/team5/hotelreservation/domain/coupon/repository/CouponRepository.java
@@ -0,0 +1,11 @@
+package daewoo.team5.hotelreservation.domain.coupon.repository;
+
+
+import daewoo.team5.hotelreservation.domain.coupon.entity.CouponEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CouponRepository extends JpaRepository<CouponEntity, Long> {
+    Optional<CouponEntity> findByCouponCode(String couponCode);
+}
