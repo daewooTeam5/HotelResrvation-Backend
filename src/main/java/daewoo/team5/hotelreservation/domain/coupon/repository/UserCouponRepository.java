56c0fb8e51de70d771c92546d253f6fd42f699a1
devaspirant0510 <seungho020510@gmail.com>
2025-09-28 22:00:00 +0900

    feat: UserCouponEntity 및 UserCouponRepository 수정: 엔티티 이름 및 테이블 이름 변경, 쿼리 수정

--- src/main/java/daewoo/team5/hotelreservation/domain/coupon/repository/UserCouponRepository.java
+++ src/main/java/daewoo/team5/hotelreservation/domain/coupon/repository/UserCouponRepository.java
@@ -29,7 +29,7 @@ public interface UserCouponRepository extends JpaRepository<UserCouponEntity,Lon
                     p.name as placeName,
                     f.url as placeImageUrl,
                     p.id as placeId
-             from UserCouponEntity uc
+             from UserCoupon uc
              join Places p on uc.coupon.place.id = p.id
              join File f on f.id = (
                  select min(f2.id)
@@ -51,7 +51,7 @@ public interface UserCouponRepository extends JpaRepository<UserCouponEntity,Lon
                     p.name as placeName,
                     f.url as placeImageUrl,
                     p.id as placeId
-             from UserCouponEntity uc
+             from UserCoupon uc
              join Places p on uc.coupon.place.id = p.id
              join File f on f.id = (
                  select min(f2.id)
@@ -73,7 +73,7 @@ public interface UserCouponRepository extends JpaRepository<UserCouponEntity,Lon
                     p.name as placeName,
                     f.url as placeImageUrl,
                     p.id as placeId
-             from UserCouponEntity uc
+             from UserCoupon uc
              join Places p on uc.coupon.place.id = p.id
              join File f on f.id = (
                  select min(f2.id)
@@ -85,7 +85,7 @@ public interface UserCouponRepository extends JpaRepository<UserCouponEntity,Lon
 
     @Query("""
             select uc.coupon
-            from UserCouponEntity uc
+            from UserCoupon uc
             where uc.user.id = :userId
             and uc.isUsed = false
             and uc.coupon.expiredAt > current_timestamp
